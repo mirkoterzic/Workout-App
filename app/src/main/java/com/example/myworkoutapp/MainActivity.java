e1e4d586e7af1f1e72bafcea97520de67985b02c
mirkoterzic <mirko.terzic2002@gmail.com>
2024-06-16 15:18:13 +0200

    added funtionality so you can change to PaceCalculatorActivity and WorkoutTrackerActivity

--- app/src/main/java/com/example/myworkoutapp/MainActivity.java
+++ app/src/main/java/com/example/myworkoutapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myworkoutapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button pace_calculator_btn;
+    private Button workout_tracker_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,25 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        pace_calculator_btn = findViewById(R.id.pace_calculator_btn);
+        workout_tracker_btn= findViewById(R.id.workout_tracker_btn);
+
+        pace_calculator_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent PaceCalculatorIntent = new Intent(MainActivity.this, PaceCalculatorActivity.class);
+                startActivity(PaceCalculatorIntent);
+                finish();
+            }
+        });
+        workout_tracker_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent WorkoutTrackerIntent= new Intent(MainActivity.this,WorkoutTrackerActivity.class);
+                startActivity(WorkoutTrackerIntent);
+                finish();
+            }
         });
     }
 }
